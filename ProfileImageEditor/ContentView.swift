56492781098905e1fa06a23a6470c0dff11689f5
Сергій <serhii@ionsot.km.ua>
2025-04-04 12:41:55 +0300

    Initial Commit

---
+++ ProfileImageEditor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProfileImageEditor
+//
+//  Created by Сергій on 04.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProfileImageEditor/ProfileImageEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileImageEditorApp.swift
+//  ProfileImageEditor
+//
+//  Created by Сергій on 04.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct ProfileImageEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
